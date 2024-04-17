01db3c8b95d9e232554bda3ab545301eb1ed105d
Vishnu M <149586311+vishnuMChanthavila@users.noreply.github.com>
2024-04-17 14:04:42 +0530

    Chore: evn Changes

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,9 +1,9 @@
 // config.js
 
 const config = {
-  // apiUrl: "http://51.20.108.121:3000",
+  apiUrl: "http://51.20.108.121:3000",
   // apiUrl: 'http://192.168.29.81:3000',
-  apiUrl: 'http://192.168.29.61:3000',
+  // apiUrl: 'http://192.168.29.61:3000',
 
   // Replace this with your actual API URL
 };
